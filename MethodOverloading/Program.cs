9e43b4ce271b644b5c2977a65f0abe5bd984d0a9
Henry Nguyen <nguyen.henry796@gmail.com>
2023-01-17 00:31:34 -0500

    method overloading made

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace MethodOverloading
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 10, false));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool total)
+        {
+            int sum = Add(num1, num2);
+            if (total)
+            {
+                if (sum == 1)
+                {
+                    return sum.ToString() + " dollar";
+                }
+                else
+                {
+                    return sum.ToString() + " dollars";
+                }
+            }
+            else
+            {
+                return sum.ToString();
+            }   
         }
     }
 }
